b3e6d613ca4c40c3c4c3f078e9f65d54b759e656
sasihara <sasihara@mvg.biglobe.ne.jp>
2020-10-03 15:40:33 +0900

    Add project files.

---
+++ thinkerV2/externalThinker.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "messageParser.hpp"
+
+void HandleInformationRequest(MessageParser messageParser, SOCKET sock, struct sockaddr_in from, int sockaddr_in_size);
+void HandleThinkRequest(MessageParser messageParser, SOCKET sock, struct sockaddr_in from, int sockaddr_in_size);
+int SendThinkAccept(int id, SOCKET sock, struct sockaddr_in from, int sockaddr_in_size);
+int SendThinkResponse(int id, unsigned char x, unsigned char y, SOCKET sock, struct sockaddr_in from, int sockaddr_in_size);
